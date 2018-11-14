60a1f9d9bd94150f58ca41ea04f200814a99ca18
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-11-14 22:45:27 +0100

    feat(nginx run): enable readonly and run as root

--- run.sh
+++ run.sh
@@ -10,9 +10,10 @@ docker run \
   --restart always \
   --network="host" \
   --memory-swappiness=0 \
+  --readonly \
   --tmpfs /var/cache/nginx:rw,size=${CACHE_SIZE},uid=101,mode=1777 \
   --tmpfs /var/run:rw,size=20k,uid=101,mode=1777 \
-  -u nginx \
+  -u root \
   -v "${CERTBOT_WEBROOT_PATH}:/cert_webroot" \
   -v "${PWD}/nginx.${WISE_ENVIRONMENT_TYPE}.conf:/etc/nginx/nginx.conf:ro" \
   -v "${PWD}/conf:/conf:ro" \
