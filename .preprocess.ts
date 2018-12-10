3e13042f19e8d34604d18a395c9d0b8e60d65778
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-12-10 21:36:23 +0100

    docs: update security email to contact@wiseteam.io

---
+++ .preprocess.ts
@@ -0,0 +1,12 @@
+// this file is loaded by wise-ci preprocessor
+
+export const hooks = [];
+export const data = {
+    repository: {
+        name: "wise-proxy"
+    },
+    npm: {
+        package: "wise-proxy"
+    }
+};
+export const excludes = [ "dist", ".vscode", "coverage" ];
